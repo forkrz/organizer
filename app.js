d4fe2ccf720f4c8401af48efc514bd9040789bb5
forkrz <forkrz8@gmail.com>
2021-06-02 01:09:19 +0200

    slider finished, main paige finshed

--- app.js
+++ app.js
@@ -2,14 +2,42 @@ const slider = document.querySelector('.slider-elements');
 
 const leftArrow = document.querySelector('.left');
 const rightArrow = document.querySelector('.right');
+const indicatorParents = document.querySelector('.controls ul');
 
-var sectionIndex = 0;
+let sectionIndex = 0;
+
+document.querySelectorAll('.controls li').forEach(function(indicator, ind) {
+    indicator.addEventListener('click', function() {
+        sectionIndex = ind;
+        document.querySelector('.controls .selected').classList.remove('selected');
+        indicator.classList.add('selected');
+        slider.style.transform = 'translate(' + (sectionIndex) * -25 + '%)';
+    });
+});
 
 rightArrow.addEventListener('click', function() {
     sectionIndex = (sectionIndex < 3) ? sectionIndex + 1 : 0;
+    document.querySelector('.controls .selected').classList.remove('selected');
+    indicatorParents.children[sectionIndex].classList.add('selected');
     slider.style.transform = 'translate(' + (sectionIndex) * -25 + '%)';
 });
 leftArrow.addEventListener('click', function() {
     sectionIndex = (sectionIndex > 0) ? sectionIndex - 1 : 3;
+    document.querySelector('.controls .selected').classList.remove('selected');
+    indicatorParents.children[sectionIndex].classList.add('selected');
     slider.style.transform = 'translate(' + (sectionIndex) * -25 + '%)';
 });
+
+function changeImg() {
+    if (sectionIndex < 3) {
+        sectionIndex++;
+    } else {
+        sectionIndex = 0;
+    }
+    document.querySelector('.controls .selected').classList.remove('selected');
+    indicatorParents.children[sectionIndex].classList.add('selected');
+    slider.style.transform = 'translate(' + (sectionIndex) * -25 + '%)';
+    setTimeout("changeImg()", 2000);
+}
+
+window.onload = changeImg();
