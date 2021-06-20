7a5de9c5b6bff98fa14c7e4159f37023681afecb
forkrz <forkrz8@gmail.com>
2021-06-20 13:13:50 +0200

    main text and slider added

--- js/index.js
+++ js/index.js
@@ -10,41 +10,41 @@ const nav = document.querySelector('.navigation');
 let isMenuOpen = nav.classList.contains('navigation_open');
 
 
-// let sectionIndex = 0;
+let sectionIndex = 0;
 
-// document.querySelectorAll('.controls li').forEach(function(indicator, ind) {
-//     indicator.addEventListener('click', function() {
-//         sectionIndex = ind;
-//         document.querySelector('.controls .selected').classList.remove('selected');
-//         indicator.classList.add('selected');
-//         slider.style.transform = 'translate(' + (sectionIndex) * -25 + '%)';
-//     });
-// });
+document.querySelectorAll('.controls li').forEach(function(indicator, ind) {
+    indicator.addEventListener('click', function() {
+        sectionIndex = ind;
+        document.querySelector('.controls .selected').classList.remove('selected');
+        indicator.classList.add('selected');
+        slider.style.transform = 'translate(' + (sectionIndex) * -25 + '%)';
+    });
+});
 
-// rightArrow.addEventListener('click', function() {
-//     sectionIndex = (sectionIndex < 3) ? sectionIndex + 1 : 0;
-//     document.querySelector('.controls .selected').classList.remove('selected');
-//     indicatorParents.children[sectionIndex].classList.add('selected');
-//     slider.style.transform = 'translate(' + (sectionIndex) * -25 + '%)';
-// });
-// leftArrow.addEventListener('click', function() {
-//     sectionIndex = (sectionIndex > 0) ? sectionIndex - 1 : 3;
-//     document.querySelector('.controls .selected').classList.remove('selected');
-//     indicatorParents.children[sectionIndex].classList.add('selected');
-//     slider.style.transform = 'translate(' + (sectionIndex) * -25 + '%)';
-// });
+rightArrow.addEventListener('click', function() {
+    sectionIndex = (sectionIndex < 3) ? sectionIndex + 1 : 0;
+    document.querySelector('.controls .selected').classList.remove('selected');
+    indicatorParents.children[sectionIndex].classList.add('selected');
+    slider.style.transform = 'translate(' + (sectionIndex) * -25 + '%)';
+});
+leftArrow.addEventListener('click', function() {
+    sectionIndex = (sectionIndex > 0) ? sectionIndex - 1 : 3;
+    document.querySelector('.controls .selected').classList.remove('selected');
+    indicatorParents.children[sectionIndex].classList.add('selected');
+    slider.style.transform = 'translate(' + (sectionIndex) * -25 + '%)';
+});
 
-// function changeImg() {
-//     if (sectionIndex < 3) {
-//         sectionIndex++;
-//     } else {
-//         sectionIndex = 0;
-//     }
-//     document.querySelector('.controls .selected').classList.remove('selected');
-//     indicatorParents.children[sectionIndex].classList.add('selected');
-//     slider.style.transform = 'translate(' + (sectionIndex) * -25 + '%)';
-//     setTimeout("changeImg()", 2000);
-// }
+function changeImg() {
+    if (sectionIndex < 3) {
+        sectionIndex++;
+    } else {
+        sectionIndex = 0;
+    }
+    document.querySelector('.controls .selected').classList.remove('selected');
+    indicatorParents.children[sectionIndex].classList.add('selected');
+    slider.style.transform = 'translate(' + (sectionIndex) * -25 + '%)';
+    setTimeout("changeImg()", 2000);
+}
 
 function hamburgerDisplay() {
     nav.classList.toggle('navigation_open');
@@ -52,7 +52,7 @@ function hamburgerDisplay() {
 }
 
 
-//window.onload = changeImg();
+window.onload = changeImg();
 
 
 
